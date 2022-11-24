b11dc0355b22137e177e9cb691412aa9dfc824a0
윤태빈 <gusckd000@gmail.com>
2022-11-24 20:48:07 +0900

    검색 기능

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -43,6 +43,11 @@ public class BoardService {
         return boardRepository.findAll(pageable);
     }
 
+    public Page<Board> boardSearchList(String searchKeyword, Pageable pageable) {
+
+        return boardRepository.findByTitleContaining(searchKeyword, pageable);
+    }
+
     // 특정 게시글 불러오기
     public Board boardView(Integer id) {
 
